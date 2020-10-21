243c956ac630883af53162c2d4eba2d259d8cf4c
Cooperhsieh <cooper79216@gmail.com>
2020-10-21 15:57:52 +0800

    Initial Commit

---
+++ Cafe_in_Taiwan_API/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cafe_in_Taiwan_API
+//
+//  Created by Cooper on 2020/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Cafe_in_Taiwan_APITests/Cafe_in_Taiwan_APITests.swift
@@ -0,0 +1,33 @@
+//
+//  Cafe_in_Taiwan_APITests.swift
+//  Cafe_in_Taiwan_APITests
+//
+//  Created by Cooper on 2020/10/21.
+//
+
+import XCTest
+@testable import Cafe_in_Taiwan_API
+
+class Cafe_in_Taiwan_APITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
